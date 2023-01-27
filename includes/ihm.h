5717b77db34297b518c4c42e57df6506bac17837
leorouxx <leo.roux@etu.minesparis.psl.eu>
2023-01-27 13:01:16 +0100

    ihm fonctionnelle (récupère les inputs)

---
+++ includes/ihm.h
@@ -0,0 +1,10 @@
+#include <vector>
+#include <map>
+#include <string>
+
+int keyEvent();
+bool exists(std::map<char,char>, char);
+char getInput(std::vector<char>);
+void delay(const int&);
+bool existsBis(std::map<char,std::string> dict, char key);
+void action(char);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#include <vector>
+#include "ihm.h"
 
-int main(int, char**) {
+int main()
+{
+    bool gameIsRunning{true};
+    char termInput;
+    char input;
+    std::vector<char> buffer;
+
+    while(gameIsRunning)
+    {
+
+        //Input gathering
+
+        delay(500);
+        while(keyEvent())
+        {
+            std::cin >> termInput; 
+            //std::cout << "\n";
+            buffer.push_back(termInput);
+        }
+
+        input = getInput(buffer);
+        buffer.clear();
+
+        // Event management
+        //std::cout << "input envoyé :" << input << "\n";
+
+        action(input);
+
+        if (input == 'x')
+        {
+            gameIsRunning = false;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
